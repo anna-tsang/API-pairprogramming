f33c1071ad3ae560fdccb0e72c33b36324637193
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-07 19:47:15 +0800

    [API pair programming][Anna] feat: add company

--- src/main/java/com/afs/restapi/CompanyController.java
+++ src/main/java/com/afs/restapi/CompanyController.java
@@ -1,4 +1,22 @@
 package com.afs.restapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("company")
 public class CompanyController {
+    public CompanyRepository companyRepository;
+
+    public CompanyController(CompanyRepository companyRepository){
+        this.companyRepository = companyRepository;
+    }
+
+    @GetMapping
+    public List<Company> getCompany(){
+        return companyRepository.findAll();
+    }
 }
--- src/main/java/com/afs/restapi/CompanyRepository.java
+++ src/main/java/com/afs/restapi/CompanyRepository.java
@@ -1,4 +1,33 @@
 package com.afs.restapi;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class CompanyRepository {
+    private List<Company> companyList = new ArrayList<>();
+
+    public CompanyRepository(){
+        List<Employee> employeeListOfCompanyA = new ArrayList<>();
+        employeeListOfCompanyA.add(new Employee (1,"Anna",20,"F",5000));
+        employeeListOfCompanyA.add(new Employee (2,"Johnson",20,"M",4000));
+        employeeListOfCompanyA.add(new Employee (3,"Apple",20,"F",4000));
+        companyList.add(new Company(1, "Anna Ltd",  employeeListOfCompanyA));
+
+        List<Employee> employeeListOfCompanyB = new ArrayList<>();
+        employeeListOfCompanyB.add(new Employee (1,"Kandy",20,"F",5000));
+        employeeListOfCompanyB.add(new Employee (2,"Raymon",20,"M",4000));
+        companyList.add(new Company(2, "Bnnb Ltd", employeeListOfCompanyB));
+
+        List<Employee> employeeListOfCompanyC = new ArrayList<>();
+        employeeListOfCompanyC.add(new Employee (1,"Bnnb",20,"F",5000));
+        employeeListOfCompanyC.add(new Employee (2,"Cnnc",20,"M",4000));
+        employeeListOfCompanyC.add(new Employee (3,"Dnnd",20,"F",4000));
+        companyList.add(new Company(3, "Cnnc Ltd", employeeListOfCompanyC));
+    }
+    public List<Company> findAll() {
+        return companyList;
+    }
 }

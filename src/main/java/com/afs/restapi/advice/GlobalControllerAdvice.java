6211f6df68723d920a323a68830b11d871314770
Johnson Kwok <kwokjo2@oocl.com>
2021-12-08 21:41:56 +0800

    [Company][Johnson Kwok] feat: handle CompanyNotFoundException in advice

--- src/main/java/com/afs/restapi/advice/GlobalControllerAdvice.java
+++ src/main/java/com/afs/restapi/advice/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.afs.restapi.advice;
 
+import com.afs.restapi.exception.CompanyNotFoundException;
 import com.afs.restapi.exception.EmployeeNotFoundException;
 import com.afs.restapi.exception.NoMatchIdFoundException;
 import org.springframework.http.HttpStatus;
@@ -11,7 +12,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class GlobalControllerAdvice {
     @ExceptionHandler({EmployeeNotFoundException.class})
-    public ErrorResponse handleNotFound(Exception exception) {
+    public ErrorResponse handleEmployeeNotFound(Exception exception) {
         return new ErrorResponse(404, "Entity Not Found.");
     }
+
+    @ExceptionHandler({CompanyNotFoundException.class})
+    public ErrorResponse handleCompanyNotFound(Exception exception) {
+        return new ErrorResponse(404, "Company Not Found.");
+    }
+
+
 }

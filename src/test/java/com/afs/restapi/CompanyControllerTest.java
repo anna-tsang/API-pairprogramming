985c30db1a29e98d5b49359d543adc93f3864322
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-08 13:49:09 +0800

    [restapi-test][Anna] test: should_return_company_list_when_perform_get_given_companies

--- src/test/java/com/afs/restapi/CompanyControllerTest.java
+++ src/test/java/com/afs/restapi/CompanyControllerTest.java
@@ -1,14 +1,20 @@
 package com.afs.restapi;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CompanyControllerTest {
+    public static final String COMPANIES_ENDPOINT = "/companies";
     @Autowired
     MockMvc mockMvc;
 
@@ -22,5 +28,18 @@ public class CompanyControllerTest {
     @BeforeEach
     void cleanRepository(){
         companyRepository.clearAll();
+    }
+
+    @Test
+    void should_return_company_list_when_perform_get_given_companies() throws Exception {
+        //given
+        Company company = new Company(1, "Anna Ltd",null);
+        companyRepository.create(company);
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get(COMPANIES_ENDPOINT))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect((jsonPath("$[0].companyName").value("Anna Ltd")));
     }
 }

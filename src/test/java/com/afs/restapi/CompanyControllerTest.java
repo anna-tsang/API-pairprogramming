df8f642cd9908010857d05ecd3dfb88db14580d6
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-08 14:09:21 +0800

    [restapi-test][Anna] test:should_return_employee_list_when_perform_get_given_company_id

--- src/test/java/com/afs/restapi/CompanyControllerTest.java
+++ src/test/java/com/afs/restapi/CompanyControllerTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -59,5 +62,26 @@ public class CompanyControllerTest {
                 .andExpect(jsonPath("$.id").isNumber())
                 .andExpect((jsonPath("$.companyName").value("Anna Ltd")))
                 .andExpect((jsonPath("$.employees").value(IsNull.nullValue())));
+    }
+
+    @Test
+    void should_return_employee_list_when_perform_get_given_company_id() throws Exception {
+        //given
+        Employee employee = new Employee(1,"Anna", 20,"M", 20);
+        List<Employee> employees = Arrays.asList(employee);
+        Company company = new Company(1, "Anna Ltd",employees);
+        companyRepository.create(company);
+        Company company2 = new Company(2, "Anna Company",null);
+        companyRepository.create(company2);
+
+        //when
+        //then
+        mockMvc.perform((MockMvcRequestBuilders.get(COMPANIES_ENDPOINT + "/{id}/employees", company.getId())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect((jsonPath("$[0].name")).value("Anna"))
+                .andExpect((jsonPath("$[0].age").value(20)))
+                .andExpect((jsonPath("$[0].gender").value("M")))
+                .andExpect((jsonPath("$[0].salary").value(20)));
     }
 }

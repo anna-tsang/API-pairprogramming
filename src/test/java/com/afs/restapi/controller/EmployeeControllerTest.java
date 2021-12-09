882a18971b4d4b6a22e2dcb736f59d9a7e0b8b4c
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-09 20:43:10 +0800

    [mongodb][Anna] feat: employee controller test - should_delete_employee_when_perform_delete_given_employee_id

--- src/test/java/com/afs/restapi/controller/EmployeeControllerTest.java
+++ src/test/java/com/afs/restapi/controller/EmployeeControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,10 +37,10 @@ public class EmployeeControllerTest {
     void cleanRepositoryBefore(){
         mockEmployeeRepositoryNew.deleteAll();;
     }
-    @AfterEach
-    void cleanRepositoryAfter(){
-        mockEmployeeRepositoryNew.deleteAll();;
-    }
+//    @AfterEach
+//    void cleanRepositoryAfter(){
+//        mockEmployeeRepositoryNew.deleteAll();;
+//    }
 
     @Test
     void should_get_all_employees_when_perform_get_given_employees() throws Exception {
@@ -145,39 +146,40 @@ public class EmployeeControllerTest {
                 .andExpect(jsonPath("$[2].age").value(20));
 
     }
-//
-//    @Test
-//    void should_return_updated_employee_when_perform_put_given_updated_employee() throws Exception {
-//        //given
-//        Employee employeeAnna = new Employee("1","Anna", 20,"F", 99999, "1");
-//        employeeRepository.create(employeeAnna);
-//        String updatedEmployee = "{\n" +
-//                "        \"name\": \"Anna\",\n" +
-//                "        \"age\": 20,\n" +
-//                "        \"gender\": \"F\",\n" +
-//                "        \"salary\": 2021\n" +
-//                "    }";
-//        //when
-//        mockMvc.perform(MockMvcRequestBuilders.put(EMPLOYEE_ENDPOINT + "/{id}",employeeAnna.getId())
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(updatedEmployee))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(1))
-//                .andExpect(jsonPath("$.name").value("Anna"))
-//                .andExpect(jsonPath("$.gender").value("F"))
-//                .andExpect(jsonPath("$.age").value(20))
-//                .andExpect(jsonPath("$.salary").value(2021));
-//    }
-//
-//    @Test
-//    void should_delete_employee_when_perform_delete_given_employee_id() throws Exception {
-//        //given
-//        Employee employeeAnna = new Employee("1","Anna", 20,"F", 99999, "1");
-//        employeeRepository.create(employeeAnna);
-//
-//        //when
-//        //then
-//        mockMvc.perform(MockMvcRequestBuilders.delete(EMPLOYEE_ENDPOINT+"/{id}", employeeAnna.getId()))
-//                .andExpect(status().isNoContent());
-//    }
+
+    @Test
+    void should_return_updated_employee_when_perform_put_given_updated_employee() throws Exception {
+        //given
+        Employee employeeAnna = new Employee("1","Anna", 20,"F", 99999, "1");
+        mockEmployeeRepositoryNew.insert(employeeAnna);
+        String updatedEmployee = "{\n" +
+                "        \"id\": \"1\",\n" +
+                "        \"name\": \"Anna\",\n" +
+                "        \"age\": 29,\n" +
+                "        \"gender\": \"F\",\n" +
+                "        \"salary\": \"20\",\n" +
+                "    }";
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.put(EMPLOYEE_ENDPOINT + "/{id}",employeeAnna.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(updatedEmployee))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Anna"))
+                .andExpect(jsonPath("$.gender").value("F"))
+                .andExpect(jsonPath("$.age").value(20));
+    }
+
+    @Test
+    void should_delete_employee_when_perform_delete_given_employee_id() throws Exception {
+        //given
+        Employee saveEmployee = mockEmployeeRepositoryNew.save(new Employee(null,"Anna", 20,"F", 99999, "1"));
+
+        //when
+        //then
+        System.out.printf("/employees/" + saveEmployee.getId());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/employees/" + saveEmployee.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+    }
 }

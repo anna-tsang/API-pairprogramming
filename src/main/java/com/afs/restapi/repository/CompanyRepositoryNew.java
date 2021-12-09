c967bf28cc7f43f8d2a6ab0054740eb8d1305772
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-09 21:09:05 +0800

    [mongodb][Anna] feat: companyRepositoryNew

--- src/main/java/com/afs/restapi/repository/CompanyRepositoryNew.java
+++ src/main/java/com/afs/restapi/repository/CompanyRepositoryNew.java
@@ -1,4 +1,13 @@
 package com.afs.restapi.repository;
 
-public interface CompanyRepositoryNew {
+import com.afs.restapi.entity.Company;
+import com.afs.restapi.entity.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CompanyRepositoryNew extends MongoRepository<Company, String> {
+    List<Employee> findAllByCompanyId(String companyId);
 }

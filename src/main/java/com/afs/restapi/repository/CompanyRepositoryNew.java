683db1e2869eb96bb8625737d9ef40967c7ef563
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-10 10:44:08 +0800

    [mongodb][Anna] feat: company controller test - remove unused code

--- src/main/java/com/afs/restapi/repository/CompanyRepositoryNew.java
+++ src/main/java/com/afs/restapi/repository/CompanyRepositoryNew.java
@@ -5,9 +5,7 @@ import com.afs.restapi.entity.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CompanyRepositoryNew extends MongoRepository<Company, String> {
-//    List<Employee> findAllByCompanyId(String companyId);
+
 }
